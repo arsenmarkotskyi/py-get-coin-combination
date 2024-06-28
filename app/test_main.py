f6cd91936a71f8eb7c678bfa70f24f0c1115eb16
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-06-28 16:59:52 +0300

    "sol"

--- app/test_main.py
+++ app/test_main.py
@@ -12,5 +12,5 @@ from app.main import get_coin_combination
         (50, [0, 0, 0, 2])
     ]
 )
-def test_modify_classes(cents: int, coins: list) -> None:
+def test_modify_classes(cents: int, coins: list[int]) -> None:
     assert get_coin_combination(cents) == coins
